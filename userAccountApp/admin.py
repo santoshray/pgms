89489dff280164d6b2a8f28da9cbd39c03afe001
Santosh Ray <skray@qti.qualcomm.com>
2018-12-18 20:34:41 -0800

    Added the Models and migrated them to the sqlite3 database . The models are also inlcuded in the admin.py so that they can be accessed by the Django Admin
    Signed-off-by: Santosh Ray <santosh.ray81@gmail.com>

--- userAccountApp/admin.py
+++ userAccountApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from userAccountApp.models import IdentityTypeRecord,AddressRecord,PersonalInfoRecord,UserAccountRecord,IdVerificationRecord,CompanyInfoRecord,EmergencyContactRecord
 # Register your models here.
+
+admin.site.register(IdentityTypeRecord)
+admin.site.register(AddressRecord)
+admin.site.register(PersonalInfoRecord)
+admin.site.register(UserAccountRecord)
+admin.site.register(IdVerificationRecord)
+admin.site.register(CompanyInfoRecord)
+admin.site.register(EmergencyContactRecord)
--- userAccountApp/models.py
+++ userAccountApp/models.py
@@ -21,21 +21,49 @@ class AddressRecord(models.Model):
     def __str__(self):
         return  str(self.__dict__)
 
-
-
-class UserAccountRecord(models.Model):
+class PersonalInfoRecord(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=254,default="nothing",null=True)
     last_name  = models.CharField(max_length=254,default="nothing",null=True)
     middle_name = models.CharField(max_length=254,default="nothing",null=True)
     email = models.EmailField(max_length=254,null=True)
     phone = models.CharField(max_length=20,default="99999999",null=True)
+
+
+class UserAccountRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    userInfo = models.ForeignKey(PersonalInfoRecord,related_name="userInfo",null=True,on_delete=models.CASCADE)
     permanentAddress = models.ForeignKey(AddressRecord,related_name="permanentAddress",null=True,on_delete=models.CASCADE)
     mailingAddress = models.ForeignKey(AddressRecord,related_name="mailingAddress",null=True,on_delete=models.CASCADE)
     profilePhoto = models.FileField(upload_to='profile_pics/', default='profile_pics/None/no-img.jpg' )
+
+    def __str__(self):
+      return  str(self.__dict__)
+
+
+class IdVerificationRecord(models.Model):
+    id = models.AutoField(primary_key=True)
     idType = models.ForeignKey(IdentityTypeRecord ,related_name="identityType",null=True,on_delete=models.CASCADE)
     idDocs = models.FileField(upload_to='profile_data/', default='profile_data/None/file' )
 
+    def __str__(self):
+      return  str(self.__dict__)
+
+class CompanyInfoRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    companyName = models.CharField(max_length=254,default="nothing",null=True)
+    companyAddress = models.ForeignKey(AddressRecord,related_name="companyAddress",null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+      return  str(self.__dict__)
+
+
+class EmergencyContactRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    contactInfo1 = models.ForeignKey(PersonalInfoRecord,related_name="emergencyInfo1",null=True,on_delete=models.CASCADE)
+    relationship1 = models.CharField(max_length=254,default="nothing",null=True)
+    contactInfo2 = models.ForeignKey(PersonalInfoRecord,related_name="emergencyInfo2",null=True,on_delete=models.CASCADE)
+    relationship2 = models.CharField(max_length=254,default="nothing",null=True)
 
     def __str__(self):
       return  str(self.__dict__)

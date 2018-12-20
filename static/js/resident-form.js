86530241ddbbabf43f165528d022aedbf1296253
anita <anita.ani2007@gmail.com>
2018-12-19 21:41:43 -0800

    Implemented other links in the Resident Form page

--- static/js/resident-form.js
+++ static/js/resident-form.js
@@ -8,14 +8,9 @@ navItemIdVerification.addEventListener('click', showIdVerification);
 navItemCompanyInfo.addEventListener('click', showCompanyInfo);
 navItemEmergencyContact.addEventListener('click', showEmergencyContact);
 let pgManagementProfile = document.querySelector('.pg-management-profile');
-let pgManagementProfileStatus = pgManagementProfile.style.display;
 let pgManagementIdVerification = document.querySelector('.pg-management-idVerification');
-let pgManagementIdVerificationStatus = pgManagementIdVerification.style.display;
 let pgManagementCompanyInfo = document.querySelector('.pg-management-companyInfo');
-let pgManagementCompanyInfoStatus = pgManagementCompanyInfo.style.display;
 let pgManagementEmergencyContact = document.querySelector('.pg-management-emergencyContact');
-let pgManagementEmergencyContactStatus = pgManagementEmergencyContact.style.display;
-let displayLink = "";
 
 hideall();
 function hideall(){
@@ -25,51 +20,19 @@ function hideall(){
   pgManagementEmergencyContact.style.display = "none";
 
 }
-/*
-function checkDisplayEachLink(){
-  if(pgManagementProfile.style.display == "block"){
-//    displayLink = pgManagementProfile.style.display;
-//    pgManagementProfile.style.display = "none";
-  }
-  if(pgManagementIdVerification.style.display == "block"){
-  //  displayLink = pgManagementIdVerificationStatus;
-    pgManagementIdVerification.style.display = "none";
-  }
-  if(pgManagementIdVerification.style.display == "block"){
-    // displayLink = pgManagementCompanyInfoStatus;
-    pgManagementIdVerification.style.display = "none";
-  }
-  if(pgManagementEmergencyContact.style.display == "block"){
-    // displayLink = pgManagementEmergencyContactStatus;
-    pgManagementEmergencyContact.style.display = "none";
-  }
-}
-*/
-
 function showProfile(){
- console.log("showProfile called");
   hideall();
   pgManagementProfile.style.display="block";
 }
 function showIdVerification(){
-// checkDisplayEachLink();
-console.log("showIdVerification called");
-
   hideall();
   pgManagementIdVerification.style.display="block";
-//  pgManagementIdVerification.style.display = "none";
-
 }
 function showCompanyInfo(){
-//  checkDisplayEachLink();
-  console.log("showCompanyInfo called");
   hideall();
   pgManagementCompanyInfo.style.display="block";
 }
 function showEmergencyContact(){
-//  checkDisplayEachLink();
-  console.log("showEmergencyContact called");
-
   hideall();
   pgManagementEmergencyContact.style.display="block";
 }
